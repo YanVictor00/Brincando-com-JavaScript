e26fb3a81e5150f9df0c8e5d3baa543c63c7380a
Yan Victor <yanvictor799@gmail.com>
2024-12-06 22:53:02 -0300

    Mais simulação ksks

--- letras.js
+++ letras.js
@@ -1,3 +1,16 @@
+//criando variável
+let nome = "Yan"
+nome = "Yan Victor"
+document.getElementById("titulo").textContent = "Olá " + nome
+
+
+//criando uma function
+function mudarNome(){
+    let nome = "Yan Victor"
+    document.getElementById("msgsaudacao").textContent = "Olá, " + nome
+}
+
+
 //simulação de troca de text de h1,p,a...
 document.getElementById("h1").textContent = "alterando Titulo"
 document.getElementById("p").textContent = "alterando Parágrafo"
