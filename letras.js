0ff8f0266c0891929002dd59a1c7560dd78d0692
Yan Victor <yanvictor799@gmail.com>
2024-12-06 22:39:45 -0300

    simulacao de troca de text JS

---
+++ letras.js
@@ -0,0 +1,4 @@
+//simulação de troca de text de h1,p,a...
+document.getElementById("h1").textContent = "alterando Titulo"
+document.getElementById("p").textContent = "alterando Parágrafo"
+document.getElementById("a").textContent = "nome para link"
