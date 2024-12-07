b4ba2e2209b854a40e2e913b84107bc3a4648edc
Yan Victor <yanvictor799@gmail.com>
2024-12-06 22:34:28 -0300

    Function e variável
    Alterando uma mensagem de saudação com um click

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+let nome = "Yan"
+nome = "Yan Victor"
+document.getElementById("titulo").textContent = "Olá " + nome
+*/
+let nome = "Yan Victor"
+function mudarNome(){
+    document.getElementById("msgsaudacao").textContent = "Olá, " + nome
+}
