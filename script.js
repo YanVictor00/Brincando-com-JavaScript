2434a6dbb282b8defce53cece8f4f8d18cc8b642
Yan Victor <yanvictor799@gmail.com>
2024-12-07 22:05:20 -0300

    fazendo uma lista nao ordenada JS

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-/*
-let nome = "Yan"
-nome = "Yan Victor"
-document.getElementById("titulo").textContent = "Olá " + nome
-*/
-let nome = "Yan Victor"
-function mudarNome(){
-    document.getElementById("msgsaudacao").textContent = "Olá, " + nome
+function adicionarTarefa() {
+    //variavel mensagem recebe "tarefa..."
+    let mensagem = "Tarefa adicionada com sucesso!";
+    //variavel inputTarefa recebe id inputTarefa (o input do html)
+    let inputTarefa = document.getElementById("inputTarefa");
+    //variavel tarefa recebe o valor da variavel inputTarefa e remove os espaço inicial e final.
+    let tarefa = inputTarefa.value.trim();
+    //se tarefa for diferente do string vazio
+    if (tarefa !== "") {
+        //id mensagem recebe um texto da variável mensagem
+        document.getElementById("mensagem").textContent = mensagem;
+        //variavel listaTarefas recebe id listaTarefas (a lista não ordenada do html)
+        let listaTarefas = document.getElementById("listaTarefas");
+        //variavel novaTarefa cria uma li no html 
+        let novaTarefa = document.createElement("li");
+
+        //variavel novaTarefa recebe variavel tarefa
+        novaTarefa.textContent = tarefa;
+
+        //variavel listaTarefas acrescenta um elemento filho na (novaTarefa)
+        listaTarefas.appendChild(novaTarefa);
+
+        //mostrar no console (tarefa)
+        console.log(tarefa);
+        //inputTarefa recebe um espaço vazio como valor
+        inputTarefa.value = "";
+    }
+    //se não 
+    else {
+        //id mensagem irá adicionar um text (Por favor...)
+        document.getElementById("mensagem").textContent = "Por favor, insira uma tarefa válida!";
+    }
 }
