9fa3c55a6b1000bcb90948ce500853a9347715da
Yan Victor <yanvictor799@gmail.com>
2025-01-02 20:12:32 -0300

    Condição Aninhada

--- rascunho.js
+++ rascunho.js
@@ -139,3 +139,18 @@ function multa(){
         msg.innerText = "DIRIJA COM CUIDADO!"
     }
     }
+
+
+
+//condição aninhada
+let idade = 70
+console.log(`Você tem ${idade} anos.`);
+
+if(idade < 16){
+    console.log("NÃO VOTA!");
+}else if(idade < 18 || idade > 65){
+    console.log("VOTO OPCIONAL.");
+}else{
+    console.log("VOTO OBRIGATÓRIO!");
+
+}
