4ca05d5e34fd59bb757a85da884b6d266fd41f45
Yan Victor <yanvictor799@gmail.com>
2025-01-09 23:03:34 -0300

    Detectando genero e idade

--- Exercicios/IdSite/id.js
+++ Exercicios/IdSite/id.js
@@ -3,22 +3,20 @@ function verificar()
     let data = new Date()
     let ano = data.getFullYear()
     let fano = document.getElementById('txtano')
-    let res = document.querySelector('div#res')
-    let fsex = document.getElementById('radsex')
+    let res = document.querySelector('p#res')
+    let fsex = document.getElementsByName('radsex')
     let idade = ano - Number(fano.value)
     let genero = ''
     
-    if(fsex[0].checked) {
-        genero = "Homem"
-    }else if(fsex[1].checked){
-        genero = "Mulher"
-    }
-    console.log(genero);
-    
-    if(fano.value.length == 0 || Number(fano.value) > ano){
+    if(fano.value == 0 || Number(fano.value) > ano){
         window.alert("Verifique os dados e tente novamente!")
     }else{
-        res.innerHTML = `Idade caulculada: ${idade}`
+        if (fsex[0].checked) {
+        genero = "Homem"
+        }else if (fsex[1].checked){
+        genero = "Mulher"
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
         fano.value = ""
     }
     
