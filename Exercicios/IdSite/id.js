f99f39db37a69ce4b978d2729e9ed480242869bb
Yan Victor <yanvictor799@gmail.com>
2025-01-10 22:25:09 -0300

    Analisando o código

--- Exercicios/IdSite/id.js
+++ Exercicios/IdSite/id.js
@@ -1,18 +1,34 @@
-function verificar()
-{
+function verificar()//funçao chamada verificar()
+
+{   //variavel data recebe nova data
     let data = new Date()
+    //variavel ano recebe data do sistema 
     let ano = data.getFullYear()
+    //variavel fano recebe input da data de nascimento
     let fano = document.getElementById('txtano')
+    //variavel res recebe paragrafo com id res
     let res = document.querySelector('p#res')
+    //variavel fsex recebe radio de suxualidade
     let fsex = document.getElementsByName('radsex')
+    //variavel idade recebe ano - valor do input de nascimento
     let idade = ano - Number(fano.value)
+    //variavel genero recebe string
     let genero = ''
+    //variavel recebe elemento com id img
     let img = document.getElementById('img')
+
+    //se a data de nascimento tiver 0 caracteris OU data de nascimento for menor que o ano atual
     if(fano.value.length == 0 || Number(fano.value) > ano){
+        //abrir um alerta com uma msg
         window.alert("Verifique os dados e tente novamente!")
-    }else{
+    }
+    //se não
+    else{
+        //se radio sexualidade tiver marcado homem
         if (fsex[0].checked) {
+            //variavel genero recebe "Homem"
             genero = "Homem"
+            //ifs sobre idade
             if(idade >= 0 && idade < 2){
                 img.src = 'fotos/bebeH.png'
             }else if( idade < 14){
@@ -25,9 +41,11 @@ function verificar()
                 img.src = 'fotos/idosoH.jpg'
             }
 
-
+        //se não se radio sexualidade tiver marcado mulher
         }else if (fsex[1].checked){
+            //variavel genero recebe "Mulher"
             genero = "Mulher"
+            //ids sobre idade
             if(idade >= 0 && idade < 2){
                 img.src = 'fotos/bebeM.jpg'
             }else if( idade < 14){
@@ -41,13 +59,16 @@ function verificar()
             }
 
         }
+        //res recebe um nvo documento html...
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        //variavel da data de nascimento recebe um valor vazio
         fano.value = ""
+        //res acrescenta as imgs
         res.appendChild(img)
         
     }
+    //debugar idade
     console.log(idade);
+    //debugar genero
     console.log(genero);
-    
-    
 }
