e1fc788a7d79353d3a0df8d616386632bd341241
Yan Victor <yanvictor799@gmail.com>
2025-01-11 22:57:30 -0300

    Colocando uma surpresa

--- Exercicios/ModeDark/dark.js
+++ Exercicios/ModeDark/dark.js
@@ -3,6 +3,8 @@ function multa(){
     let txtvelv = txtvel.value
     let msg = document.querySelector('p#msg')
     //console.log(`Sua velocidade atual é de <strong>${txtvelv}</strong>Km/h`)
+    let img = document.querySelector('img#img')
+    
     if(txtvelv > 60){
         msg.innerText = "VOCÊ ULTRAPASSOU A VELOCIDADE MAXIMA. MULTADO!"
     }else{
@@ -12,11 +14,17 @@ function multa(){
     
     function alterar(){
         let form = document.getElementsByName('cori')
+        let a = document.querySelector('p#a')
         if(form[0].checked) {
             document.body.style.background = "black"
             document.body.style.color = "white"
+            img.src = 'surprise.png'
+            img.style.width = "800px"
+            a.appendChild(img)
         }else if(form[1].checked) {
             document.body.style.background = "white"
-             document.body.style.color = "black"
+            document.body.style.color = "black"
+            img.src = ''
         }
+
     }
