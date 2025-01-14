e1184221eed52da8d04dd05b72d414a1acd05087
Yan Victor <yanvictor799@gmail.com>
2025-01-14 00:48:39 -0300

    Fazendo a base

--- Exercicios/Contador/script.js
+++ Exercicios/Contador/script.js
@@ -1,13 +1,13 @@
 function contar(){
-    //let n1 = document.querySelector('input#n1')
-    //let n2 = document.querySelector('input#n2')
-    //let passos = document.querySelector('input#passos')
+    let n1 = document.querySelector('input#n1')
+    let n2 = document.querySelector('input#n2')
+    let passos = document.querySelector('input#passos')
+    let msg = document.querySelector('p#msg')
 
-    
-    let n2 = 10
-    let passos = 2
-    for(n1 = 1; n1 <= n2; n1 += passos){
+    for(n1 = Number(n1.value); n1 = Number(n2.value); n1 += Number(passos.value)){
         console.log(n1);
-        
+        msg.innerHTML = n1
+
     }
+
 }
