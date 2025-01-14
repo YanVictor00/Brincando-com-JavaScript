5fe86d7238f1f28cb9659762be6274a7350026ea
Yan Victor <yanvictor799@gmail.com>
2025-01-14 00:24:54 -0300

    Fechei sem querer

---
+++ Exercicios/Contador/script.js
@@ -0,0 +1,13 @@
+function contar(){
+    //let n1 = document.querySelector('input#n1')
+    //let n2 = document.querySelector('input#n2')
+    //let passos = document.querySelector('input#passos')
+
+    
+    let n2 = 10
+    let passos = 2
+    for(n1 = 1; n1 <= n2; n1 += passos){
+        console.log(n1);
+        
+    }
+}
