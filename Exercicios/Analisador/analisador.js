2850cecf6e4c5bb9698a8ec5daa43c2c05777d0d
Yan Victor <yanvictor799@gmail.com>
2025-01-22 20:39:59 -0300

    Análise do código

--- Exercicios/Analisador/analisador.js
+++ Exercicios/Analisador/analisador.js
@@ -1,23 +1,37 @@
+//numero
 let num = document.querySelector('input#inpNum')
+//lista
 let lista = document.querySelector('select#valores')
+//div para add msg
 let res = document.querySelector('div#res')
+//Array que vai armazenar os numeros
 let valores = []
 
+
+// função que verifica se o numero é entre 1 e 100
 function isNumero(n){
+    //se o N for maior ou igual a 1 e N for menor ou igual a 100
     if( Number(n) >= 1 && Number(n) <= 100){
+        //rwturna true
         return true
     }else{
-        return false
-    }
-}
-function inLista(n, l){
-    if(l.indexOf(Number(n)) != -1){
-        return true
-    }else{
+        //se não retornar falso
         return false
     }
 }
 
+//função que verifica se o número está na Array
+function inLista(n, l){
+    //se o indice de N for diferente de -1 na array
+    if(l.indexOf(Number(n)) != -1){
+        //retornar verdadeiro
+        return true
+    }else{
+        //se não retornar false
+        return false
+    }
+}
+//função para adicionar o número na Array
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)){
         valores.push(Number(num.value))
@@ -31,7 +45,7 @@ function adicionar(){
     num.value = ""
     num.focus()
 }
-
+//função para limpar o número
 function clean(){
     valores = []
     lista.innerHTML = ``
@@ -40,7 +54,7 @@ function clean(){
     lista.appendChild(msg)
      res.innerHTML = ""
 }
-
+//função para analisar os numeros na Array
 function finalizar(){
     let total = valores.length
     let maior = valores[0]
